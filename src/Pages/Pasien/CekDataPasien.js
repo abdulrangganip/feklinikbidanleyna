831372850e492af01e8768f86c2bafd7bf2b5a04
abdulrangganip <80730537+abdulrangganip@users.noreply.github.com>
2023-07-04 10:49:59 +0700

    Update CekDataPasien.js

--- src/Pages/Pasien/CekDataPasien.js
+++ src/Pages/Pasien/CekDataPasien.js
@@ -84,7 +84,9 @@ const CekDataPasien = () => {
           </div>
         </div>
         <div className="card bg-white">
-          <p className="font-bold text-xl text-center py-4">Data Keluarga Ibu</p>
+          <p className="font-bold text-xl text-center py-4">
+            Data Keluarga Ibu
+          </p>
           <hr />
           <div className="card-body grid grid-cols-2 gap-x-5">
             <div className="flex flex-col gap-y-5">
@@ -141,146 +143,9 @@ const CekDataPasien = () => {
         </div>
         <div className="mt-4">
           <button class="btn btn-wide mr-3">
-            <Link to="/cek-data-pasien/data-kehamilan-pasien">KEHAMILAN DAN MELAHIRKAN</Link>
-          </button>
-          <button class="btn btn-wide">
-            <Link to="/cek-data-pasien/data-kb-pasien">KELUARGA BERENCANA</Link>
-          </button>
-        </div>
-        <div className="card bg-white my-5">
-          <p className="font-bold text-xl text-center py-4">Data Diri Pasien</p>
-          <hr />
-          <div className="card-body grid grid-cols-2 gap-x-5">
-            <div className="flex flex-col gap-y-5">
-              <div className="grid grid-cols-2">
-                <label className="font-bold">NO RM</label>
-                <label className="input input-info">011-05-LYN-2022</label>
-              </div>
-              <div className="grid grid-cols-2">
-                <label className="font-bold">NAMA LENGKAP</label>
-                <label className="input input-info">DINDA NURFAUZI</label>
-              </div>
-              <div className="grid grid-cols-2">
-                <label className="font-bold">TEMPAT LAHIR</label>
-                <label className="input input-info">BANDUNG</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">TANGGAL LAHIR</label>
-                <label className="input input-info">8 JANUARI 1999</label>
-              </div>
-              <div className="grid grid-cols-2">
-                <label className="font-bold">UMUR</label>
-                <label className="input input-info">24</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">ALAMAT</label>
-                <label className="input input-info">JLN MELATI NO 23</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">AGAMA </label>
-                <label className="input input-info">ISLAM</label>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-y-5">
-              <div className="grid grid-cols-2">
-                <label className="font-bold">GOLONGAN DARAH</label>
-                <label className="input input-info">O</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">NO NIK</label>
-                <label className="input input-info">3273160801990004</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">NO KK</label>
-                <label className="input input-info">3273163211323320</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">RIYAWAT PENYAKIT</label>
-                <label className="input input-info">-</label>
-              </div>
-              <div className="grid grid-cols-2">
-                <label className="font-bold">RIWAYAT ALERGI</label>
-                <label className="input input-info">GATAL GATAL</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">PEKERJAAN</label>
-                <label className="input input-info">WIRASWASTA</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">PENDIDIKAN TERAKHIR</label>
-                <label className="input input-info">SMA</label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card bg-white">
-          <p className="font-bold text-xl text-center py-4">Data Keluarga Ibu</p>
-          <hr />
-          <div className="card-body grid grid-cols-2 gap-x-5">
-            <div className="flex flex-col gap-y-5">
-              <div className="grid grid-cols-2">
-                <label className="font-bold">NAMA SUAMI</label>
-                <label className="input input-info">REINALDI FAUZI</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">UMUR SUAMI</label>
-                <label className="input input-info">26</label>
-              </div>
-              <div className="grid grid-cols-2">
-                <label className="font-bold">TEMPAT LAHIR SUAMI</label>
-                <label className="input input-info">BANDUNG</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">TANGGAL LAHIR SUAMI</label>
-                <label className="input input-info">8 JANUARI 1997</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">AGAMA</label>
-                <label className="input input-info">ISLAM</label>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-y-5">
-              <div className="grid grid-cols-2">
-                <label className="font-bold">PENDIDIKAN SUAMI</label>
-                <label className="input input-info">DIPLOMA III</label>
-              </div>
-              <div className="grid grid-cols-2">
-                <label className="font-bold">PEKERJAAN SUAMI</label>
-                <label className="input input-info">BUMN</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">GOL DARAH SUAMI</label>
-                <label className="input input-info">O</label>
-              </div>
-              <div className="grid grid-cols-2">
-                <label className="font-bold">JUMLAH ANAK </label>
-                <label className="input input-info">1</label>
-              </div>
-
-              <div className="grid grid-cols-2">
-                <label className="font-bold">UMUR ANAK TERAKHIR</label>
-                <label className="input input-info">5 TAHUN</label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mt-4">
-          <button class="btn btn-wide mr-3">
-            <Link to="/cek-data-pasien/data-kehamilan-pasien">KEHAMILAN DAN MELAHIRKAN</Link>
+            <Link to="/cek-data-pasien/data-kehamilan-pasien">
+              KEHAMILAN DAN MELAHIRKAN
+            </Link>
           </button>
           <button class="btn btn-wide">
             <Link to="/cek-data-pasien/data-kb-pasien">KELUARGA BERENCANA</Link>
